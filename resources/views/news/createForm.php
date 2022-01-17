0465d6031f7098c1fa6407ae29b4f17f75dac098
Nikolay <nikolay.vanzhin@yandex.ru>
2022-01-17 17:23:41 +0500

    hw2.2.6 update

--- resources/views/news/createForm.php
+++ resources/views/news/createForm.php
@@ -1,12 +1,14 @@
 <h2>Создание новости</h2>
 <div>
     <?php foreach ($news as $key => $item):?>
-        <div>
-            <p>Поле: <?=$key?></p>
-            <input type="text" name="<?=$key?>">
-        </div>
-
+        <p>Поле: <?=$key?></p>
+        <input type="text" name="<?=$key?>">
     <?php endforeach;?>
+    <p>Поле для подробного описания новости: </p>
+    <textarea name="full_description"
+              rows="10" cols="50" >
+    </textarea>
+    <br>
     <br>
    <input type="submit" value="добавить новость" name="submit">
 

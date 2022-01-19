7bbfb1de2aa9f6d738cec820cd9b0542926df7cb
Nikolay <nikolay.vanzhin@yandex.ru>
2022-01-19 11:19:31 +0500

    HW3 prepare

---
+++ resources/views/news/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.main')
+@section('header')
+    <section class="py-5 text-center container">
+        <div class="row py-lg-5">
+            <div class="col-lg-6 col-md-8 mx-auto">
+                <h1 class="fw-light">Все новости</h1>
+
+            </div>
+        </div>
+    </section>
+@endsection
+@section('content')
+    @forelse($newsList as $news)
+        <div class="col">
+            <div class="card shadow-sm">
+                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">{{ $news['title'] }}</text></svg>
+
+                <div class="card-body">
+                    <p class="card-text">{!! $news['description'] !!}</p>
+                    <p class="card-text"> Автор: {{ $news['author'] }}</p>
+                    <p class="card-text"> {{ $news['created_at'] }}</p>
+
+
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <a href="{{ route('news.show',['id' => $news['id']]) }}" type="button" class="btn btn-sm btn-outline-secondary">Подробнее</a>
+                            <a href="{{ route('news.categories.show', ['category_id' => $news['category_id']]) }}" type="button" class="btn btn-sm btn-outline-secondary">Все новости раздела</a>
+                        </div>
+                        <small class="text-muted">9 mins</small>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    @empty
+        <h2>Записей нет</h2>
+
+    @endforelse
+@endsection
+
--- resources/views/news/show.php
+++
@@ -1,9 +0,0 @@
-<div>
-
-    <h2><?=$news['title']?></h2>
-    <h3><?=$news['description']?></h3>
-    <p><?=$news['author'] . " "?><?=$news['created_at']?></p>
-
-    <a href="/news">к новостям</a>
-
-</div>

a502fbcd59202cf794fcd31d3deea8a4963cd778
Nikolay <nikolay.vanzhin@yandex.ru>
2022-02-02 16:30:47 +0500

    feedback migration created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             NewsSeeder::class,
             NewsCategoriesSeeder::class,
+            FeedbacksSeeder::class,
         ]);
     }
 }

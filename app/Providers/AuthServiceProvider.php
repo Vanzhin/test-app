840b8ec75de4b517505d107716e939a5e796faf2
Nikolay <nikolay.vanzhin@yandex.ru>
2022-02-25 11:30:40 +0500

    mail password reset created

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -29,11 +30,20 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            dd($notifiable,$url);
             return (new MailMessage)
                 ->subject('Подтверждение электронного адреса на сайте ' . env('APP_NAME'))
                 ->line('Нажмите на кнопку для подтверждения учетной записи.')
                 ->greeting('Привет, ' . $notifiable->name)
                 ->action('Подтвердите адрес почты', $url);
+        });
+        ResetPassword::toMailUsing(function ($notifiable, $token) {
+            $url = env('APP_URL') . '/password/reset/' . $token . '?email=' .  urlencode($notifiable->email);
+            return (new MailMessage)
+                ->subject('Сброс пароля пользователя ' . $notifiable->name . ' на сайте '. env('APP_NAME'))
+                ->line('Нажмите на кнопку для сброса пароля.')
+                ->greeting('Привет, ' . $notifiable->name)
+                ->action('Сбросить пароль', $url);
         });
 
         //
